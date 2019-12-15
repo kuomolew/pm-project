701fbe1b6552884a433964cdc0b72da3feb2c7e3
kuomolew <kuomolew@gmail.com>
2019-12-15 22:25:18 +0200

    supreme

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(() => {
     console.log('start');
+
+    $('.main-banner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        customPaging : function(slider, i) {
+            return '<img src="img/empty-dot.png" /><img src="img/full-dot.png" />';
+        },
+    });
 });
